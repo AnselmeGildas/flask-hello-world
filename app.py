2e7a025b1d89840b354a5e108305c4f5b98a266f
kadioanselme <kadioanselme@gmail.com>
2023-09-30 18:51:57 +0000

    test1

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-# Shamelessly copied from http://flask.pocoo.org/docs/quickstart/
+# Download the helper library from https://www.twilio.com/docs/python/install
+import os
+from twilio.rest import Client
 
-from flask import Flask
-app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = 'AC784e6326d5053cdd3e4049f7d932092f'
+auth_token = '3a56ba2bc5366c3c196393879143674d'
+client = Client(account_sid, auth_token)
 
-if __name__ == '__main__':
-    app.run()
+message = client.messages \
+    .create(
+         from_='+12565784749',
+         body='/tHello world',
+         to='+2250788914994'
+     )
+
+print(message.body)
